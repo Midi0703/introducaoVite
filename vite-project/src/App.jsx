df11513f9d9916db88d05bee784e51a1b8362c48
Midiel <midielcastroramires400@gmail.com>
2026-08-05 22:46:51 -0300

    Aula 5/8

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>Exemplo parágrafo</p>
+      <h2>Exemplo h2</h2>
+      <h3>Exemplo h3</h3>
+      <label htmlFor = "">Exemplo label</label>
+    </div>
+  )
+}
+
+export default App
