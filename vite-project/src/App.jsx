cd9e9d64e84badf1a9ecf474b76704a9c42a3be5
Midiel <midielcastroramires400@gmail.com>
2026-08-07 21:33:33 -0300

    Aula 6/8

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
-import './App.css'
+import styles from './App.module.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,7 +11,7 @@ function App() {
     <div>
       <h1>Home</h1>
       <p>Exemplo parágrafo</p>
-      <h2>Exemplo h2</h2>
+      <h2 className={styles.titulo2}>Exemplo h2</h2>
       <h3>Exemplo h3</h3>
       <label htmlFor = "">Exemplo label</label>
     </div>
