995ba3a9b4d955a4ab1d127e95a111c6153ab94d
Midiel <midielcastroramires400@gmail.com>
2026-08-20 19:29:12 -0300

    correcao 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,21 @@
-import { Routes, Route } from 'react-router-dom';
-
-import Home from './pages/home'; 
-import Atividades from './pages/atividades';
-import Exemplos from './pages/exemplos';
-
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import styles from './App.module.css'
 
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
-  <Routes>
-    
-    <Route path="/" element={<Home />} />
-
-    <Route path="/atividades" element={<Atividades />} />
-    <Route path="/exemplos" element={<Exemplos />} />
-
-
-  </Routes>
+    <div>
+      <h1>Home</h1>
+      <p>Exemplo parágrafo</p>
+      <h2 className={styles.titulo2}>Exemplo h2</h2>
+      <h3>Exemplo h3</h3>
+      <label htmlFor = "">Exemplo label</label>
+    </div>
   )
 }
 
-export default App;
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import './css/reset.css'
 import './css/global.css'
-
 import App from './App.jsx'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
